675f97b2a3b2b6ce27116031ffa4bdbbd847dd3c
arturogood17 <arturo_good17@hotmail.com>
2025-03-14 14:31:41 +0100

    Test read() from config dir

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/arturogood17/aggreGator/internal/config"
+)
+
 func main() {
-	//Does something
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(cfg.DbURL)
+		fmt.Println(cfg.CurrentUserName)
+	}
 }

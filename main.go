49d8757f3c1a3c2599ad72b03d2dea5ae8f172d2
arturogood17 <arturo_good17@hotmail.com>
2025-03-15 21:31:44 +0100

    Change to main.go to add args

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/arturogood17/aggreGator/internal/config"
 )
@@ -11,8 +12,24 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		cfg.SetUser("nstapt")
-		fmt.Println(cfg.DbURL)
-		fmt.Println(cfg.CurrentUserName)
+		S := state{
+			cfg: &cfg,
+		}
+		comnds := commands{
+			cmds: make(map[string]func(*state, command) error),
+		}
+		comnds.register("login", handlerLogin)
+		if len(os.Args) < 2 {
+			fmt.Println("invalid input")
+			os.Exit(1)
+		}
+		comnd := command{
+			name: os.Args[1],
+			args: os.Args[2:],
+		}
+		if err := comnds.run(&S, comnd); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 }

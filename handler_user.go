2912765f44f49c9b8c12c3474bbca637a380d35a
arturogood17 <arturo_good17@hotmail.com>
2025-03-16 23:28:32 +0100

    Added registerlogin func and db check in handlerlogin func

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"time"
+
+	"github.com/arturogood17/aggreGator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -9,10 +15,40 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("usage: cmd <name>")
 	}
 	username := cmd.args[0]
+	if _, err := s.db.GetUser(context.Background(), username); err != nil {
+		fmt.Println("User login not in database")
+		os.Exit(1)
+		return err
+	}
 	err := s.cfg.SetUser(username)
 	if err != nil {
 		return fmt.Errorf("error setting user: %w", err)
 	}
 	fmt.Println("User set successfully.")
+	return nil
+}
+
+func registerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("usage: cmd <name>")
+	}
+	_, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err == nil {
+		fmt.Printf("Error: User '%s' already exists\n", cmd.args[0])
+		os.Exit(1)
+	}
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      cmd.args[0]})
+	if err != nil {
+		return err
+	}
+	s.cfg.SetUser(user.Name)
+	fmt.Println(user.ID)
+	fmt.Println(user.CreatedAt)
+	fmt.Println(user.UpdatedAt)
+	fmt.Println(user.Name)
 	return nil
 }

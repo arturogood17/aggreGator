a249be79b8325ac79658b5efbff03d13d1486a8a
arturogood17 <arturo_good17@hotmail.com>
2025-03-22 00:21:45 +0100

    Changed agg, added scrapefeeds and scrapefeed

--- Aggre_func.go
+++ Aggre_func.go
@@ -2,22 +2,76 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/arturogood17/aggreGator/internal/database"
+	"github.com/google/uuid"
 )
 
 func Aggregation(s *state, cmd command) error {
-	rss, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if len(cmd.args) < 1 || len(cmd.args) > 2 {
+		return fmt.Errorf("usage: %v <time_between_reqs>", cmd.name)
+	}
+	aggTime, err := time.ParseDuration(cmd.args[0])
 	if err != nil {
 		return err
 	}
-	fmt.Println(rss.Channel.Title)
-	fmt.Println(rss.Channel.Link)
-	fmt.Println(rss.Channel.Description)
-	for _, v := range rss.Channel.Item {
-		fmt.Println(v.Title)
-		fmt.Println(v.Link)
-		fmt.Println(v.Description)
-		fmt.Println(v.PubDate)
+	log.Printf("Collecting feeds every: %v", aggTime)
+
+	ticker := time.NewTicker(aggTime)
+
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
 	}
-	return nil
+}
+
+func scrapeFeeds(s *state) {
+	feedToScrape, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	scrapeFeed(s.db, feedToScrape)
+}
+
+func scrapeFeed(db *database.Queries, feed database.Feed) {
+	if _, err := db.MarkFeedFetched(context.Background(), feed.ID); err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println(feed.Url)
+	fetched, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	for _, v := range fetched.Channel.Item {
+		published, err := time.Parse(time.RFC1123, v.PubDate)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       v.Title,
+			Url:         v.Link,
+			Description: sql.NullString{String: v.Description},
+			PublishedAt: sql.NullTime{Time: published},
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+				continue
+			}
+			log.Printf("Couldn't create post")
+			continue
+		}
+	}
+	log.Printf("Feed %s collected, %v posts found", feed.Name, len(fetched.Channel.Item))
 }

1e46d3101f10749397f729061533a98620ea755e
arturogood17 <arturo_good17@hotmail.com>
2025-04-13 05:57:19 +0200

    Go code generated from sqlc

--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -48,3 +48,36 @@ func (q *Queries) CreateFeed(ctx context.Context, arg CreateFeedParams) (Feed, e
 	)
 	return i, err
 }
+
+const feedList = `-- name: FeedList :many
+SELECT name, url, user_id FROM feeds
+`
+
+type FeedListRow struct {
+	Name   string
+	Url    string
+	UserID uuid.UUID
+}
+
+func (q *Queries) FeedList(ctx context.Context) ([]FeedListRow, error) {
+	rows, err := q.db.QueryContext(ctx, feedList)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []FeedListRow
+	for rows.Next() {
+		var i FeedListRow
+		if err := rows.Scan(&i.Name, &i.Url, &i.UserID); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
